9f7498c35c3fb23cbe7c266a43b13fb29b83a545
arslan070898 <arslan.070898@gmail.com>
2020-08-16 11:20:47 +0500

    adding starting files

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: Math.random().toString(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks = [...this.tasks, task];
+    return task;
+  }
+}
